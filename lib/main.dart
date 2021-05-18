6bcfeb097cd850c51df1ac79e8b322a1e42cb807
Otavio Ho Aragoni <otavio.aragoni@gmail.com>
2021-05-18 16:31:30 -0300

    Add logic to city selection

--- lib/main.dart
+++ lib/main.dart
@@ -13,95 +13,103 @@ final List<Map> cities = [
   {"city": "São Paulo", "woeid": 455827}
 ];
 
-final List<Map> consolidatedWeather = [{
-"id": 6686199684005888,
-"weather_state_name": "Heavy Rain",
-"weather_state_abbr": "hr",
-"wind_direction_compass": "W",
-"created": "2021-05-18T15:32:02.751719Z",
-"applicable_date": "2021-05-18",
-"min_temp": 6.17,
-"max_temp": 14.29,
-"the_temp": 14.5,
-"wind_speed": 5.421905916174872,
-"wind_direction": 261,
-"air_pressure": 1013,
-"humidity": 70,
-"visibility": 10.60245665314563,
-"predictability": 77
-},
-{
-"id": 6686199684005888,
-"weather_state_name": "Heavy Rain",
-"weather_state_abbr": "hr",
-"wind_direction_compass": "W",
-"created": "2021-05-18T15:32:02.751719Z",
-"applicable_date": "2021-05-18",
-"min_temp": 6.17,
-"max_temp": 14.29,
-"the_temp": 14.5,
-"wind_speed": 5.421905916174872,
-"wind_direction": 261,
-"air_pressure": 1013,
-"humidity": 70,
-"visibility": 10.60245665314563,
-"predictability": 77
-},
-{
-"id": 6686199684005888,
-"weather_state_name": "Heavy Rain",
-"weather_state_abbr": "hr",
-"wind_direction_compass": "W",
-"created": "2021-05-18T15:32:02.751719Z",
-"applicable_date": "2021-05-18",
-"min_temp": 6.17,
-"max_temp": 14.29,
-"the_temp": 14.5,
-"wind_speed": 5.421905916174872,
-"wind_direction": 261,
-"air_pressure": 1013,
-"humidity": 70,
-"visibility": 10.60245665314563,
-"predictability": 77
-},
-{
-"id": 6686199684005888,
-"weather_state_name": "Heavy Rain",
-"weather_state_abbr": "hr",
-"wind_direction_compass": "W",
-"created": "2021-05-18T15:32:02.751719Z",
-"applicable_date": "2021-05-18",
-"min_temp": 6.17,
-"max_temp": 14.29,
-"the_temp": 14.5,
-"wind_speed": 5.421905916174872,
-"wind_direction": 261,
-"air_pressure": 1013,
-"humidity": 70,
-"visibility": 10.60245665314563,
-"predictability": 77
-},
-{
-"id": 6686199684005888,
-"weather_state_name": "Heavy Rain",
-"weather_state_abbr": "hr",
-"wind_direction_compass": "W",
-"created": "2021-05-18T15:32:02.751719Z",
-"applicable_date": "2021-05-18",
-"min_temp": 6.17,
-"max_temp": 14.29,
-"the_temp": 14.5,
-"wind_speed": 5.421905916174872,
-"wind_direction": 261,
-"air_pressure": 1013,
-"humidity": 70,
-"visibility": 10.60245665314563,
-"predictability": 77
-},
+final List<Map> consolidatedWeather = [
+  {
+    "id": 6686199684005888,
+    "weather_state_name": "Heavy Rain",
+    "weather_state_abbr": "hr",
+    "wind_direction_compass": "W",
+    "created": "2021-05-18T15:32:02.751719Z",
+    "applicable_date": "2021-05-18",
+    "min_temp": 6.17,
+    "max_temp": 14.29,
+    "the_temp": 14.5,
+    "wind_speed": 5.421905916174872,
+    "wind_direction": 261,
+    "air_pressure": 1013,
+    "humidity": 70,
+    "visibility": 10.60245665314563,
+    "predictability": 77
+  },
+  {
+    "id": 6686199684005888,
+    "weather_state_name": "Heavy Rain",
+    "weather_state_abbr": "hr",
+    "wind_direction_compass": "W",
+    "created": "2021-05-18T15:32:02.751719Z",
+    "applicable_date": "2021-05-18",
+    "min_temp": 6.17,
+    "max_temp": 14.29,
+    "the_temp": 14.5,
+    "wind_speed": 5.421905916174872,
+    "wind_direction": 261,
+    "air_pressure": 1013,
+    "humidity": 70,
+    "visibility": 10.60245665314563,
+    "predictability": 77
+  },
+  {
+    "id": 6686199684005888,
+    "weather_state_name": "Heavy Rain",
+    "weather_state_abbr": "hr",
+    "wind_direction_compass": "W",
+    "created": "2021-05-18T15:32:02.751719Z",
+    "applicable_date": "2021-05-18",
+    "min_temp": 6.17,
+    "max_temp": 14.29,
+    "the_temp": 14.5,
+    "wind_speed": 5.421905916174872,
+    "wind_direction": 261,
+    "air_pressure": 1013,
+    "humidity": 70,
+    "visibility": 10.60245665314563,
+    "predictability": 77
+  },
+  {
+    "id": 6686199684005888,
+    "weather_state_name": "Heavy Rain",
+    "weather_state_abbr": "hr",
+    "wind_direction_compass": "W",
+    "created": "2021-05-18T15:32:02.751719Z",
+    "applicable_date": "2021-05-18",
+    "min_temp": 6.17,
+    "max_temp": 14.29,
+    "the_temp": 14.5,
+    "wind_speed": 5.421905916174872,
+    "wind_direction": 261,
+    "air_pressure": 1013,
+    "humidity": 70,
+    "visibility": 10.60245665314563,
+    "predictability": 77
+  },
+  {
+    "id": 6686199684005888,
+    "weather_state_name": "Heavy Rain",
+    "weather_state_abbr": "hr",
+    "wind_direction_compass": "W",
+    "created": "2021-05-18T15:32:02.751719Z",
+    "applicable_date": "2021-05-18",
+    "min_temp": 6.17,
+    "max_temp": 14.29,
+    "the_temp": 14.5,
+    "wind_speed": 5.421905916174872,
+    "wind_direction": 261,
+    "air_pressure": 1013,
+    "humidity": 70,
+    "visibility": 10.60245665314563,
+    "predictability": 77
+  },
 ];
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   // This widget is the root of your application.
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int selectedCity = cities[0]["woeid"];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -109,8 +117,23 @@ class MyApp extends StatelessWidget {
         body: Container(
           child: Column(
             children: [
-              Wrap(children: [Center(child: CitySelectionList(cities: cities))]),
-              Wrap(children: [DailyForecastList(consolidatedWeatherArray: consolidatedWeather,)])
+              Wrap(children: [
+                Center(
+                    child: CitySelectionList(
+                  cities: cities,
+                  selectedCity: selectedCity,
+                  onSelectCity: (woeid) {
+                    setState(() {
+                      selectedCity = woeid;
+                    });
+                  },
+                ))
+              ]),
+              Wrap(children: [
+                DailyForecastList(
+                  consolidatedWeatherArray: consolidatedWeather,
+                )
+              ])
             ],
           ),
         ),
--- lib/widgets/CitySelectionList.dart
+++ lib/widgets/CitySelectionList.dart
@@ -1,9 +1,36 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
-class CitySelectionList extends StatelessWidget {
-  CitySelectionList({Key? key, required this.cities}) : super(key: key);
+class CitySelectionList extends StatefulWidget {
+  CitySelectionList(
+      {Key? key,
+      required this.cities,
+      required this.selectedCity,
+      required this.onSelectCity})
+      : super(key: key);
 
   final List<Map> cities;
+  final int selectedCity;
+  final Function onSelectCity;
+
+  @override
+  _CitySelectionListState createState() => _CitySelectionListState();
+}
+
+class _CitySelectionListState extends State<CitySelectionList> {
+  int hoveredCity = -1;
+
+  void _onHover(int woeid) {
+    setState(() {
+      hoveredCity = woeid;
+    });
+  }
+
+  void _onExitHover() {
+    setState(() {
+      hoveredCity = -1;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,13 +38,38 @@ class CitySelectionList extends StatelessWidget {
       padding: const EdgeInsets.all(50),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: cities
-            .map((city) => Container(
-                  decoration: BoxDecoration(
-                      border: Border.all(color: Colors.grey, width: 3)),
-                  child: Padding(
-                    padding: const EdgeInsets.all(20),
-                    child: Center(child: Text("${city["city"]}")),
+        children: widget.cities
+            .map((city) => GestureDetector(
+                  onTap: () {
+                    widget.onSelectCity(city["woeid"]);
+                  },
+                  child: MouseRegion(
+                    onEnter: (event) {
+                      _onHover(city["woeid"]);
+                    },
+                    onExit: (eveny) {
+                      _onExitHover();
+                    },
+                    child: Container(
+                      decoration: BoxDecoration(
+                        border: Border.all(color: Colors.grey, width: 3),
+                        color: this.widget.selectedCity == city["woeid"]
+                            ? Colors.grey
+                            : Colors.white,
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(20),
+                        child: Center(
+                          child: Text(
+                            "${city["city"]}",
+                            style: TextStyle(
+                                decoration: this.hoveredCity == city["woeid"]
+                                    ? TextDecoration.underline
+                                    : TextDecoration.none),
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
                 ))
             .toList(),
